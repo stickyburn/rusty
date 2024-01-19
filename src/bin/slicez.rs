bbabc8654560f8d8d0185884f869bbbf773382a1
stickburn <yashank@hey.com>
2024-01-19 22:49:30 +0530

    feat: slices

---
+++ src/bin/slicez.rs
@@ -0,0 +1,18 @@
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn main() {
+    let s = String::from("hello world");
+    let word = first_word(&s);
+
+    println!("first string end {word}");
+}
